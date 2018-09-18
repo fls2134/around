7094993a8e39d0a7aabab3291c8331d185a5813a
YoonRyeol <yunyul3@gmail.com>
2018-09-18 21:19:44 +0900

    Merge branch 'master' of https://github.com/fls2134/around
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    push구현

--- app/src/main/java/com/example/root/appcontest/model/LocalData.java
+++ app/src/main/java/com/example/root/appcontest/model/LocalData.java
@@ -2,13 +2,14 @@ package com.example.root.appcontest.model;
 
 public class LocalData {
 
-    String title;
-    double longtitude;
-    double latitude;
-    String uptime;
-    String img_url;
-    String tag;
-    String content;
-    short data_type;
+    public String title;
+    public double longtitude;
+    public double latitude;
+    public int sYear, sMonth, sDay;
+    public int eYear, eMonth, eDay;
+    public String img_url;
+    public String tag;
+    public String content;
+    public int data_type;
 
 }
