a87e11ddbc5388cc2b465772eff3abff01c1127c
YoonRyeol <yunyul3@gmail.com>
2018-09-29 22:13:33 +0900

    Merge branch 'master' of https://github.com/fls2134/around
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/root/appcontest/activity/MainActivity.java
+++ app/src/main/java/com/example/root/appcontest/activity/MainActivity.java
@@ -114,15 +114,15 @@ public class MainActivity extends AppCompatActivity {
         data_array = new ArrayList<>();
         getServerDatas();
         // 최초 화면 설정
-        Intent Service = new Intent(getApplicationContext(), AlarmService.class);
-        bindService(Service, mConnection, Context.BIND_AUTO_CREATE);
+      //  Intent Service = new Intent(getApplicationContext(), AlarmService.class);
+     //   bindService(Service, mConnection, Context.BIND_AUTO_CREATE);
         // 최초 화면 설정
         Timer timer = new Timer(true);
         timer.schedule(new TimerTask(){
             public void run(){
-                mService.myServiceFunc();
+     //           mService.myServiceFunc();
             }
-        },1000);
+        },3000);
     }
 
     private int requestPermission()
